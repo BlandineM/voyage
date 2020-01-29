03566e85be51ddec6975247623737c24b59eb6ca
Blandine <blandine.mallart@gmail.com>
2020-01-29 18:18:51 +0100

    homepage static

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./style/homepage.scss";
 function Homepage() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="homepage">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css?family=Indie+Flower|Lobster&display=swap');
+      </style>
+      <h1>Viens Chercher Bonheur !</h1>
+      <div>
+        <h2>Tu ne sais pas où et quand partir en vacances</h2>
+      </div>
+      <div className="card">
+        <div className="sun">
+          <h3>Soleil - Plage ?</h3>
+          <img src="/plage.jpg"></img>
+        </div>
+        <div className="hiking">
+          <h3>Randonnée?</h3>
+          <img src="/randonnee.jpg"></img>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Homepage;
