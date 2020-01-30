315727fc8115813797d7eb43e36cb23feeac032b
Blandine <blandine.mallart@gmail.com>
2020-01-30 17:29:56 +0100

    add component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
 import Plage from "./components/Plage";
 import Randonnee from "./components/Randonnee";
+import Month from "./components/Month";
 import { Switch, Route } from "react-router-dom";
-
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/plage" component={Plage} />
+        <Route path="/01" component={Month} />
         <Route path="/randonnee" component={Randonnee} />
       </Switch>
       <footer>
