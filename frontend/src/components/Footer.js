aa181fae3c2d52adfd6a385e8af231ad6cc84f9c
Blandine <blandine.mallart@gmail.com>
2020-01-30 10:31:34 +0100

    footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style/footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <img src="/ping.png"></img>
+    </div>
+  );
+}
