192f7b71922c2e396a541f6d6ac45b6111058cd3
Blandine <blandine.mallart@gmail.com>
2020-01-30 23:23:31 +0100

    add props type

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,59 +2,59 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style/navBar.scss";
 
-export default function NavBar() {
+export default function NavBar(props) {
   return (
     <div className="month">
       <ul>
-        <Link to="/01">
+        <Link to={`/${props.place}/01`}>
           <li>JANV</li>
         </Link>
 
-        <Link to="/02">
+        <Link to={`/${props.place}/02`}>
           <li>FEVR</li>
         </Link>
 
-        <Link to="/03">
+        <Link to={`/${props.place}/03`}>
           <li>MAR</li>
         </Link>
 
-        <Link to="/04">
+        <Link to={`/${props.place}/04`}>
           <li>AVR</li>
         </Link>
 
-        <Link to="/05">
+        <Link to={`/${props.place}/05`}>
           <li>MAI</li>
         </Link>
 
-        <Link to="/06">
+        <Link to={`/${props.place}/06`}>
           <li>JUIN</li>
         </Link>
 
-        <Link to="/07">
+        <Link to={`/${props.place}/07`}>
           <li>JUIL</li>
         </Link>
 
-        <Link to="/08">
+        <Link to={`/${props.place}/08`}>
           <li>AOU</li>
         </Link>
 
-        <Link to="/09">
+        <Link to={`/${props.place}/09`}>
           <li>SEPT</li>
         </Link>
 
-        <Link to="/10">
+        <Link to={`/${props.place}/10`}>
           <li>OCT</li>
         </Link>
 
-        <Link to="/11">
+        <Link to={`/${props.place}/11`}>
           <li>NOV</li>
         </Link>
 
-        <Link to="/12">
+        <Link to={`/${props.place}/12`}>
           <li>DEC</li>
         </Link>
 
-        <Link to="/plage">
+        <Link to={`/${props.place}`}>
           <li>TOUT</li>
         </Link>
       </ul>
