bc6a5582ef5803bc126ed3d3183d2657e9ec1102
Blandine <blandine.mallart@gmail.com>
2020-01-30 15:48:23 +0100

    component plage

--- frontend/src/components/Plage.jsx
+++ frontend/src/components/Plage.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./style/plage.scss";
 
 function Plage() {
-  return <h1>hello world plage</h1>;
+  const [pays, setpays] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:5000/plage").then(({ data }) => {
+      setpays(data);
+    });
+  }, [setpays]);
+  return (
+    <div className="main">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css?family=Indie+Flower|Lobster&display=swap');
+      </style>
+      <h1>Let's go to the beach ! </h1>
+      <div className="destinations">
+        {pays.map((pays, i) => {
+          return (
+            <div key={i} className="cards">
+              <h2>{pays.pays}</h2>
+              <div className="image">
+                <img
+                  className="destination"
+                  src={`/destinations/${pays.id}.jpg`}
+                  alt={`${pays.pays}`}
+                ></img>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Plage;
