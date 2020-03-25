d56b3af59955ba5436562cae2099c03b01465330
Blandine <blandine.mallart@gmail.com>
2020-03-25 18:51:12 +0100

    feat(passport) add a auth file for login and sign up

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,18 @@ const port = 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const initScript = require("./initScript/initdb")
+const passport = require("passport");
 
 /* ------------------------------------------------------------ Tools */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(passport.initialize());
 
 /* -------------------------------------------------------------- Public Routes */
 
 app.use("/", require("./route/type"));
+app.use("/auth", require("./route/auth"));
 
 
 initScript.initDb()
